bed7bab785d79cc7cff95a30e3fe2adeb5a2b8db
Jatin Suri <surijatin@yahoo.com>
2023-07-31 13:52:24 -0400

    uploading

--- updateVector.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-oc project openshift-logging
-make redeploy
-oc apply -f hack/cr-vector.yaml
-rm hack/vector.toml
-sleep 4
-oc extract secrets/collector-config --keys=vector.toml --to=/home/jatinsredhat/Documents/cluster-logging-operator/hack --confirm

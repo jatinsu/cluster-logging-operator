97a677daf051b5d0ebbebb670f3cb8a654685403
Jatin Suri <surijatin@yahoo.com>
2023-08-08 13:26:39 -0400

    Attempting to do a feature gate

--- internal/constants/features.go
+++ internal/constants/features.go
@@ -6,6 +6,9 @@ const (
 	// PreviewTLSSecurityProfile is the annotation to enable TLS security profiles
 	PreviewTLSSecurityProfile = "logging.openshift.io/preview-tls-security-profile"
 
+	// OpenTelemetry is the annotation to enable OpenTelemetry output
+	OpenTelemetry = "logging.openshift.io/opentelemetry"
+
 	// UseOldRemoteSyslogPlugin use old syslog plugin (docebo/fluent-plugin-remote-syslog)
 	// +deprecated
 	UseOldRemoteSyslogPlugin = "clusterlogging.openshift.io/useoldremotesyslogplugin"

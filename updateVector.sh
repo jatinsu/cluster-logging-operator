39d1f9b19a47d6eb36c26cd2dc8a13d8168c4f68
Jatin Suri <surijatin@yahoo.com>
2023-08-25 13:04:53 -0400

    removed excess files

--- updateVector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-oc project openshift-logging
-make undeploy && make clean
-make deploy-image && make deploy-catalog && make install
-oc apply -f hack/cr-vector.yaml
-oc apply -f hack/cr-custom.yaml
-rm hack/vector.toml
-sleep 4
-oc extract secrets/collector-config --keys=vector.toml --to=/home/jatinsredhat/Documents/cluster-logging-operator/hack --confirm
